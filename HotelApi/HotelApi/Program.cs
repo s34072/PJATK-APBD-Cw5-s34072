6899ec537bea66dcff7a498e7c69b8cd5db12517
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-15 20:54:41 +0200

    Dodanie katalogów i wstępnie zaplanowany Program.cs

--- HotelApi/HotelApi/Program.cs
+++ HotelApi/HotelApi/Program.cs
@@ -1,13 +1,21 @@
+using HotelApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Dodanie obsługi klasycznych kontrolerów
+builder.Services.AddControllers();
+
+// Rejestracja naszych serwisów jako Singleton 
+builder.Services.AddSingleton<IRoomService, RoomService>();
+builder.Services.AddSingleton<IReservationService, ReservationService>();
+
+// Konfiguracja Swaggera
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfiguracja potoku żądań HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+// Zmapowanie ścieżek do naszych kontrolerów
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
